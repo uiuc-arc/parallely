60f66809397b2d866d560d5c8571e5c93d3f14bf
Keyur Joshi <kpjoshi2@illinois.edu>
2018-11-16 15:09:27 -0600

    wip pagerank go benchmarks

--- benchmarks/pagerank/pagerank.go
+++ benchmarks/pagerank/pagerank.go
@@ -1,115 +1,112 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"io/ioutil"
-	"strings"
-	// "math"
-	"time"
-	"strconv"
+  "os"
+  "fmt"
+  "io/ioutil"
+  "strings"
+  // "math"
+  "time"
+  "strconv"
 )
 
-func pagerank_func(iterations int, W [][]int, inlinks []int, outlinks []int, mystart, myend int, channel chan []float64){
-	r := 0.15
-	d := 0.85
-	// mywork := myend-mystart
-	
-	for myiteration := 0; myiteration < iterations; myiteration++{
-		pageranks := <- channel
-		for j := mystart; j<myend; j++ {
-			pageranks[j] = r
-			for k := 0; k<inlinks[j]; k++ {
-				neighbor := W[j][k]
-				pageranks[j] = pageranks[j] + d * pageranks[neighbor]/float64(outlinks[neighbor])
-			}
-		}
-		channel <- pageranks[mystart:myend]
-	}
+func pagerank_func(iterations int, W [][]int, inlinks []int, outlinks []int, mynode int, datachannel chan []float64, reschannel chan float64, datasigchannel chan bool, ressigchannel chan bool){
+  r := 0.15
+  d := 0.85
+
+  for myiteration := 0; myiteration < iterations; myiteration++{
+    <- datasigchannel
+    pageranks := <- datachannel
+    mypagerank := r
+    for k := 0; k<inlinks[mynode]; k++ {
+      neighbor := W[mynode][k]
+      //fmt.Println(mynode,myiteration,neighbor,len(pageranks),len(outlinks))
+      mypagerank += d * pageranks[neighbor]/float64(outlinks[neighbor])
+    }
+    ressigchannel <- true
+    reschannel <- mypagerank
+  }
 }
 
 func main() {
-	argsWithoutProg := os.Args[1:]
+  data_bytes, _ := ioutil.ReadFile(os.Args[1])
+  num_nodes, _ := strconv.Atoi(os.Args[2])
 
-	data_bytes, _ := ioutil.ReadFile(argsWithoutProg[0])
-	num_nodes, _ := strconv.Atoi(argsWithoutProg[1])
-	// num_edges, _ := strconv.Atoi(argsWithoutProg[2])
+  //fmt.Println("Starting reading the file")
+  data_string := string(data_bytes)
+  data_str_array := strings.Split(data_string, "\n")
 
-	fmt.Println("Starting reading the file")
-	data_string := string(data_bytes)
-	data_str_array := strings.Split(data_string, "\n")
-	
-	fmt.Println("Setting up the data structures")
-	W := make([][]int, num_nodes)
-	inlinks := make([]int, num_nodes)
-	outlinks := make([]int, num_nodes)
-	pagerank := make([]float64, num_nodes)
-	
-	for i := range W{
-		W[i] = make([]int, num_nodes)
-		inlinks[i] = 0
-		outlinks[i] = 0
-		pagerank[i] = 0.15
-	}
+  //fmt.Println("Setting up the data structures")
+  W := make([][]int, num_nodes)
+  inlinks := make([]int, num_nodes)
+  outlinks := make([]int, num_nodes)
+  pagerank := make([]float64, num_nodes)
 
-	fmt.Println("Populating the data structures")
-	// Nodes: 62586 Edges: 147892
-	for i := 1; i<len(data_str_array)-1 ; i++ {
-		// fmt.Println(data_str_array[i])
-		elements := strings.Fields(data_str_array[i])
-		// fmt.Println(elements[0])
-		index1, _ := strconv.Atoi(elements[0])
-		index2, _ := strconv.Atoi(elements[1])
-		
-		W[index2][inlinks[index2]] = index1
-		inlinks[index2]++
-		outlinks[index1]++		
-	}
+  for i := range W{
+    W[i] = make([]int, num_nodes)
+    inlinks[i] = 0
+    outlinks[i] = 0
+    pagerank[i] = 0.15
+  }
 
-	fmt.Println("Finished populating the data structures")
+  //fmt.Println("Populating the data structures")
+  for i := 1; i<len(data_str_array)-1 ; i++ {
+    elements := strings.Fields(data_str_array[i])
+    index1, _ := strconv.Atoi(elements[0])
+    index2, _ := strconv.Atoi(elements[1])
 
-	num_threads := 4
-	channels := make([]chan []float64, num_threads)
-	for i := range channels {
-		channels[i] = make(chan []float64, 100)
-	}
-	iterations := 10
+    W[index2][inlinks[index2]] = index1
+    inlinks[index2]++
+    outlinks[index1]++
+  }
 
-	// func pagerank(iterations int, W [][]int, inlinks []int, outlinks []int, mystart, myend int, channel chan []float64){
-	for i := range channels {
-		t_start := (num_nodes/num_threads) * i
-		t_end := (num_nodes/num_threads) * (i+1)
-		go pagerank_func(iterations, W, inlinks, outlinks, t_start, t_end, channels[i])
-	}
-	
-	startTime := time.Now()
-	fmt.Println("Starting the iterations")
-	for iter:=0; iter < iterations; iter++{
-		fmt.Println("Iteration : ", iter)
-		for i := range channels {
-			t_start := (num_nodes/num_threads) * i
-			t_end := (num_nodes/num_threads) * (i+1)
-			prcopy := make([]float64, len(pagerank))
-			copy(prcopy,pagerank)
-			channels[i] <- prcopy
-			results := <- channels[i]
-			fmt.Println(i, len(results), t_start, t_end, t_end-t_start)
-			
-			k := 0
-			for j:=t_start; j<t_end; j++ {
-				pagerank[j] = results[k]
-				k++
-			}
-		}
-	}
-	elapsed := time.Since(startTime)
-	fmt.Println(elapsed)
+  //fmt.Println("Finished populating the data structures")
 
-	f, _ := os.Create("output.txt")
-	defer f.Close()
+  channels := make([]chan []float64, num_nodes)
+  for i := range channels {
+    channels[i] = make(chan []float64, 1)
+  }
+  reschannels := make([]chan float64, num_nodes)
+  for i := range reschannels {
+    reschannels[i] = make(chan float64, 1)
+  }
+  sigchannels := make([]chan bool, num_nodes)
+  for i := range sigchannels {
+    sigchannels[i] = make(chan bool, 1)
+  }
+  ressigchannels := make([]chan bool, num_nodes)
+  for i := range ressigchannels {
+    ressigchannels[i] = make(chan bool, 1)
+  }
+  iterations := 10
 
-	
-	for i := range pagerank{
-		f.WriteString(fmt.Sprintln(pagerank[i]))	
-	}
+  for i := range channels {
+    go pagerank_func(iterations, W, inlinks, outlinks, i, channels[i], reschannels[i], sigchannels[i], ressigchannels[i])
+  }
+
+  //fmt.Println("Starting the iterations")
+  startTime := time.Now()
+  for iter:=0; iter < iterations; iter++{
+    //fmt.Println("Iteration : ", iter)
+    results := make([]float64, num_nodes)
+    copy(results, pagerank)
+    for i := range channels {
+      sigchannels[i] <- true
+      channels[i] <- pagerank
+    }
+    for i := range channels {
+      <- ressigchannels[i]
+      results[i] = <- reschannels[i]
+    }
+    pagerank = results
+  }
+  elapsed := time.Since(startTime)
+  fmt.Println(elapsed)
+
+  f, _ := os.Create("output.txt")
+  defer f.Close()
+
+  for i := range pagerank{
+    f.WriteString(fmt.Sprintln(pagerank[i]))
+  }
 }
