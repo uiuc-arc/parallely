6915c8406173f2c84e00a947882eb86c40ce7228
vimuth <wvf2@illinois.edu>
2019-08-31 17:34:02 -0500

    cleaning rely

--- parser/compiler.py
+++ parser/compiler.py
@@ -15,6 +15,7 @@ import copy
 import time
 from argparse import ArgumentParser
 import random
+from unroller import unrollRepeat
 
 key_error_msg = "Type error detected: Undeclared variable (probably : {})"
 
@@ -794,49 +795,6 @@ class UnrollGroups(ParallelyListener):
     #     new_name = "_" + self.current_process.getText()
     #     # self.rewriter.insertBeforeIndex(ctx.start.tokenIndex, new_name)
     #     self.rewriter.insertAfterToken(ctx.stop, new_name)
-
-
-class unrollRepeat(ParallelyListener):
-    def __init__(self, stream):
-        self.rewriter = TokenStreamRewriter.TokenStreamRewriter(stream)
-        self.replacedone = False
-
-    # def enterRepeat(self, ctx):
-    #     cs = ctx.statement().start.getInputStream()
-    #     statements = cs.getText(ctx.statement().start.start,
-    #                             ctx.statement().stop.stop)
-    #     rep_variable = int(ctx.INT().getText())
-    #     edited = ''
-    #     # removing the code for process groups
-    #     self.rewriter.delete(self.rewriter.DEFAULT_PROGRAM_NAME,
-    #                          ctx.start.tokenIndex, ctx.stop.tokenIndex)
-    #     for var in range(rep_variable):
-    #         edited += statements + ";\n"
-    #     self.rewriter.insertAfter(ctx.stop.tokenIndex, edited)
-
-    def enterRepeat(self, ctx):
-        # Do only one replacement at a time
-        if self.replacedone:
-            return
-
-        rep_variable = int(ctx.INT().getText())
-        # TODO: Is there a way to avoid string manipulation?
-        list_statements = ctx.statement()
-        cs = list_statements[0].start.getInputStream()
-        statements = cs.getText(list_statements[0].start.start,
-                                list_statements[-1].stop.stop)
-        print "------------------------------"
-        print statements
-        print "------------------------------"
-
-        new_str = ''
-        for var in range(rep_variable):
-            new_str += "  " + statements + ";\n"
-        self.rewriter.insertAfter(ctx.stop.tokenIndex + 1, new_str)
-        self.rewriter.delete(self.rewriter.DEFAULT_PROGRAM_NAME,
-                             ctx.start.tokenIndex,
-                             ctx.stop.tokenIndex + 1)
-        self.replacedone = True
 
 
 def main(program_str, outfile, filename, debug, skiprename):
