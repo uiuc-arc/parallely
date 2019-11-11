73127c1b9c1cbd56c36e4b609c6af8ed0c4c8355
vimuth <wvf2@illinois.edu>
2019-11-11 15:33:06 -0600

    Vimuth

---
+++ diesel/src/gaussian-gen/getSpeedUp.py
@@ -0,0 +1,74 @@
+import subprocess
+import re
+import numpy as np
+
+num_sample = 10
+
+times = []
+
+print "Running without dynamic tracking"
+# Compile
+commstr = """python ../../../parser/crosscompiler-diesel.py -f gaussian.par -t __basic_go.txt -o gaussian.go; go build"""
+
+result_test = subprocess.check_output(commstr, shell=True)
+print result_test
+
+for i in range(num_sample):
+    print "Running Iteration : ", i
+    result_test = subprocess.check_output("./gaussian-gen ../scale-gen/baboon.ppm temp.ppm", shell=True)
+
+    matches = re.findall("Elapsed time : .*\n", result_test)
+    time_spent = float(matches[0].split(' : ')[-1])
+    print time_spent
+    times.append(time_spent)
+
+no_track_time = np.mean(times)
+print "Runtime without tracking: ", no_track_time
+
+print "------------------------------------------"
+
+# Compile
+print "Running with dynamic tracking"
+times = []
+
+commstr = """python ../../../parser/crosscompiler-diesel.py -f gaussian.par -t __basic_go.txt -o gaussian.go -dyn; go build"""
+
+result_test = subprocess.check_output(commstr, shell=True)
+print result_test
+
+for i in range(num_sample):
+    print "Running Iteration : ", i
+    result_test = subprocess.check_output("./gaussian-gen ../scale-gen/baboon.ppm temp.ppm", shell=True)
+
+    matches = re.findall("Elapsed time : .*\n", result_test)
+    time_spent = float(matches[0].split(' : ')[-1])
+    print time_spent
+    times.append(time_spent)
+
+track_time = np.mean(times)
+print "Runtime with tracking: ", track_time
+
+print "Running with array optimization"
+times = []
+
+commstr = """python ../../../parser/crosscompiler-diesel.py -f gaussian.par -t __basic_go.txt -o gaussian.go -dyn -a -g; go build"""
+
+result_test = subprocess.check_output(commstr, shell=True)
+print result_test
+
+for i in range(num_sample):
+    print "Running Iteration : ", i
+    result_test = subprocess.check_output("./gaussian-gen ../scale-gen/baboon.ppm temp.ppm", shell=True)
+
+    matches = re.findall("Elapsed time : .*\n", result_test)
+    time_spent = float(matches[0].split(' : ')[-1])
+    print time_spent
+    times.append(time_spent)
+
+opt_time = np.mean(times)
+print "Runtime after optimizations: ", opt_time
+
+print "---------------------------------"
+print "Overhead : ", track_time / no_track_time
+print "Overhead After Optimization : ", opt_time / no_track_time
+print "---------------------------------"
