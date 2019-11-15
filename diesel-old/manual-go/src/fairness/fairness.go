ea6ea7e4677ddbfb9fa21f8e0dba3ceeea78ff40
jacob <jlaurel2@illinois.edu>
2019-11-15 17:35:38 -0600

    updated fairness

--- diesel-old/manual-go/src/fairness/fairness.go
+++ diesel-old/manual-go/src/fairness/fairness.go
@@ -1,16 +1,17 @@
 package main
 
-/*import (
-	"os"
-	"fmt"
-	"io/ioutil"
-	"strings"
-	"math"
-	"time"
-	"strconv"
-	"math/rand"
-	."dynfloats"
-)*/
+import (
+	_ "os"
+	_ "fmt"
+	_ "io/ioutil"
+	_ "strings"
+	_ "math"
+	_ "time"
+	_ "strconv"
+	_ "math/rand"
+	_ ."dynfloats"
+)
+
 
 const workers = 10
 const datasize = 10000
@@ -23,16 +24,22 @@ type DynFairness struct {
 
 func fairness_func(i int, channelin, channelout chan float32,dynchannelin,dynchannelout chan DynFairness){
      var dynamic_fairness_map map[string]DynFairness 
+	 _ = dynamic_fairness_map
 }
 
 func main() {
 
-     startTime := time.Now()
+    // startTime := time.Now()
 
      var dynamic_fairness_map map[string]DynFairness 
      var genders [datasize]float32
      var college_ranks [datasize]float32
      var years_exp [datasize]float32
+	 
+	 _ =  dynamic_fairness_map
+	 _ = genders
+	 _ = college_ranks
+	 _ = years_exp
 
      //a send and recieve channel for each worker to the master
      var channels [workers*2]chan float32
@@ -41,13 +48,17 @@ func main() {
      //make the channels
      for i := range channels {
      	 channels[i] = make(chan float32,3*datasize/workers)
-	 dynchannels[i] = make(chan DynFairness) //can change later
+	 	 dynchannels[i] = make(chan DynFairness) //can change later
      }
 
      //start the goroutines
      for i:=0; i<workers; i++ {
      	 go fairness_func(i,channels[i],channels[i+workers],dynchannels[i],dynchannels[i+workers])
      }
+  
+
+}
+
 
      
 
