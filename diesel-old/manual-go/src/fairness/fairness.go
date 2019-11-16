a68a601b709c0d14ff42535f8eb226a07e11b97e
jacob <jlaurel2@illinois.edu>
2019-11-16 15:33:02 -0600

    fixed

--- diesel-old/manual-go/src/fairness/fairness.go
+++ diesel-old/manual-go/src/fairness/fairness.go
@@ -8,60 +8,88 @@ import (
 	_ "math"
 	_ "time"
 	_ "strconv"
-	_ "math/rand"
-	_ ."dynfloats"
+	"math/rand"
+	// ."dynfloats"
 )
 
 
 const workers = 10
 const datasize = 10000
 
-type DynFairness struct {
-  epsilon float64
-  delta float64
-  
+
+type Person struct {
+	gender int
+	col_rank float64
+	years_exp float64
 }
 
-func fairness_func(i int, channelin, channelout chan float32,dynchannelin,dynchannelout chan DynFairness){
+
+func population_model() Person {
+	is_male := rand.Intn(1)
+	col_rank := rand.NormFloat64() * 10 + 25
+	var years_exp float64
+	if (is_male > 0) {
+		years_exp = rand.NormFloat64() * 5 + 15
+	} else {
+		years_exp = rand.NormFloat64() * 5 + 10
+	}
+	p := Person{is_male, col_rank, years_exp}
+	return p
+}
+
+
+func offer_job(p Person) int {
+
+	if (p.col_rank <= 5) {
+		return 1
+	} else if (p.years_exp > -5) {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func SendPersonArr(arr []Person, ind, num int, chout chan Person) {
+  for i:=0; i<num; i++ {
+    chout <- arr[ind+i]
+  }
+}
+
+func RecvPersonArr(arr []Person, num int, chin chan Person) {
+  for i:=0; i<num; i++ {
+    arr[i] = <- chin
+  }
+}
+
+
+
+func fairness_func(i int, channelin chan float32,dynchannelout chan DynFairness){
      var dynamic_fairness_map map[string]DynFairness 
-	 _ = dynamic_fairness_map
+     _ = dynamic_fairness_map
 }
 
 func main() {
 
-    // startTime := time.Now()
+    
 
-     var dynamic_fairness_map map[string]DynFairness 
-     var genders [datasize]float32
-     var college_ranks [datasize]float32
-     var years_exp [datasize]float32
-	 
-	 _ =  dynamic_fairness_map
-	 _ = genders
-	 _ = college_ranks
-	 _ = years_exp
+     var dynamic_fairness_map map[string]DynFairnessFloat 
+
+     // startTime := time.Now()
 
      //a send and recieve channel for each worker to the master
-     var channels [workers*2]chan float32
-     var dynchannels [workers*2]chan DynFairness
+     var channels [workers]chan Person	//really an array of channels (for each worker) sends the persons to each worker
+     var dynchannels [workers]chan DynFairnessFloat //channels used to send the dynamically tracked mean back to the master
 
-     //make the channels
-     for i := range channels {
-     	 channels[i] = make(chan float32,3*datasize/workers)
-	 	 dynchannels[i] = make(chan DynFairness) //can change later
+     //make/initialize the channels
+     for i := 0; i< workers; i++ {
+     	 channels[i] = make(chan Person)
+	 dynchannels[i] = make(chan DynFairnessFloat) //can change later
      }
 
      //start the goroutines
      for i:=0; i<workers; i++ {
-     	 go fairness_func(i,channels[i],channels[i+workers],dynchannels[i],dynchannels[i+workers])
+     	 go fairness_func(i,channels[i],dynchannels[i])
      }
   
-
-}
-
-
-     
-
-     
 
 }
