e197226db0774cd05f69755222dabfdd61ad9214
jacob <jlaurel2@illinois.edu>
2019-11-16 21:57:01 -0600

    saving work

--- diesel-old/manual-go/src/dynfloat_fairness/dynfloat_fairness.go
+++ diesel-old/manual-go/src/dynfloat_fairness/dynfloat_fairness.go
@@ -4,43 +4,51 @@ import "math"
 
 
 type DynFairnessFloat struct {
-  val float64
-  epsilon float64
-  delta float64
+  Val float64
+  Epsilon float64
+  Delta float64
 }
 
 
+
 func AddFloatFairness(x,y DynFairnessFloat)(ret DynFairnessFloat){
-     ret.val = x.val + y.val
-     ret.epsilon = x.epsilon + y.epsilon
-     ret.delta = x.delta + y.delta
+     ret.Val = x.Val + y.Val
+     ret.Epsilon = x.Epsilon + y.Epsilon
+     ret.Delta = x.Delta + y.Delta
      return
 }
 
 func NegFloatFairness(x DynFairnessFloat)(ret DynFairnessFloat){
-     ret.val = -x.val
-     ret.epsilon = x.epsilon
-     ret.delta = x.delta
+     ret.Val = -x.Val
+     ret.Epsilon = x.Epsilon
+     ret.Delta = x.Delta
      return
 }
 
 
 func MulFloatFairness(x,y DynFairnessFloat)(ret DynFairnessFloat){
-     ret.val = x.val * y.val
-     ret.epsilon = (math.Abs(ret.val)* y.epsilon)+(math.Abs(y.val)*x.epsilon)+(x.epsilon*y.epsilon)
-     ret.delta = x.delta + y.delta
+     ret.Val = x.Val * y.Val
+     ret.Epsilon = (math.Abs(ret.Val)* y.Epsilon)+(math.Abs(y.Val)*x.Epsilon)+(x.Epsilon*y.Epsilon)
+     ret.Delta = x.Delta + y.Delta
      return
 }
 
+
+func ConstMulFloatFairness(c float64,x DynFairnessFloat)(ret DynFairnessFloat){
+	C := DynFairnessFloat{val: c,epsilon:0;delta:0}
+	ret = MulFloatFairness(C,x)
+	return
+}
+
 func InvFloatFairness(x DynFairnessFloat)(ret DynFairnessFloat){
-     if ((x.val-x.epsilon<0)&&(x.val+x.epsilon>0)){
-     	ret.epsilon = math.Inf(1) //possible division by zero
-		ret.val = x.val
-		ret.delta = x.delta
+     if ((x.Val-x.Epsilon<0)&&(x.Val+x.Epsilon>0)){
+     	ret.Epsilon = math.Inf(1) //possible division by zero
+		ret.Val = x.Val
+		ret.Delta = x.Delta
      } else {
-     	ret.val = 1/(x.val)
-		ret.epsilon = x.epsilon/(math.Abs(x.val)*(math.Abs(x.val)-x.epsilon))
-		ret.delta = x.delta
+     	ret.Val = 1/(x.Val)
+		ret.Epsilon = x.Epsilon/(math.Abs(x.Val)*(math.Abs(x.Val)-x.Epsilon))
+		ret.Delta = x.Delta
      }
      return
 }
@@ -52,8 +60,8 @@ func DivFloatFairness(x,y DynFairnessFloat)(ret DynFairnessFloat){
 }
 
 func checkIneq(x DynFairnessFloat, eps,del float64)(ret bool){
-     ret = (x.epsilon <= eps && x.delta <= del) 
-	 return
+     ret = (x.Epsilon <= eps && x.Delta <= del) 
+     return
 }
 
 
