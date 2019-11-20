48f703f5aa0cc015bc4eec28a5e80d0fb54fde14
Keyur Joshi <kpjoshi2@illinois.edu>
2019-11-20 13:34:17 -0600

    improve scripts

--- diesel/src/scale-gen/plot_sensitivity.py
+++ diesel/src/scale-gen/plot_sensitivity.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-# data = {4: (1, 2, 3, 1, 2, 3), 8: (1, 2, 3, 1, 2, 3), 16: (1, 2, 3, 1, 2, 3)}
+benchmarkName = 'scale'
 
 data = {2: (2182202570.3, 2414151423.1, 2285356977.9, 8388608.0, 16777216.0, 8388672.0),
         4: (9446486885.3, 10332666758.0, 10303837240.5, 33554432.0, 67108864.0, 33554496.0),
@@ -8,7 +8,7 @@ data = {2: (2182202570.3, 2414151423.1, 2285356977.9, 8388608.0, 16777216.0, 838
         16: (157882553005.4, 170195137187.5, 159170102931.3, 536870912.0, 1073741824.0, 536870976.0)}
 
 names = ['Baseline', 'Unoptimized', 'Optimized']
-linestyles = ['-', '--', '-.']
+linestyles = ['-', '--', ':']
 colors = ['orange', 'red', 'green']
 
 sizes = []
@@ -17,13 +17,37 @@ comms = ([], [], [])
 
 for datum in sorted(data.keys()):
   sizes.append(datum)
-  times[0].append(data[datum][0])
-  times[1].append(data[datum][1])
-  times[2].append(data[datum][2])
-  comms[0].append(data[datum][3])
-  comms[1].append(data[datum][4])
-  comms[2].append(data[datum][5])
+  times[0].append(data[datum][0]/1e9)
+  times[1].append(data[datum][1]/1e9)
+  times[2].append(data[datum][2]/1e9)
+  comms[0].append(data[datum][3]/1e6)
+  comms[1].append(data[datum][4]/1e6)
+  comms[2].append(data[datum][5]/1e6)
 
+plt.figure(figsize=(2,2))
+plt.rcParams.update({'font.size': 12})
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+for i in range(3):
+  plt.plot(sizes,times[i],label=names[i],linestyle=linestyles[i],color=colors[i])
+# plt.legend(loc='upper left')
+plt.xlabel('Input Size')
+plt.ylabel('Time (ms)')
+plt.tight_layout()
+plt.savefig('times-{}.png'.format(benchmarkName))
+plt.close()
+
+plt.figure(figsize=(2,2))
+plt.rcParams.update({'font.size': 12})
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+for i in range(3):
+  plt.plot(sizes,comms[i],label=names[i],linestyle=linestyles[i],color=colors[i])
+# plt.legend(loc='upper left')
+plt.xlabel('Input Size')
+plt.ylabel('Data (MB)')
+plt.tight_layout()
+plt.savefig('comms-{}.png'.format(benchmarkName))
+
+'''
 # fig, axs = plt.subplots(2, 1)
 for i in range(3):
     plt.plot(sizes, times[i], label=names[i], linestyle=linestyles[i], color=colors[i])
@@ -48,3 +72,4 @@ plt.xlabel('Input Size')
 plt.xticks(sorted(data.keys()), ['2x', '4x', '8x', '16x'])
 plt.tight_layout()
 plt.savefig("memory.png")
+'''
