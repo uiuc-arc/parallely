71706878fb8bba189a72b8f593929301579bde08
Keyur Joshi <kpjoshi2@illinois.edu>
2019-11-20 13:21:26 -0600

    better plots

--- diesel-old/manual-go/src/size-sensitivity-bscholes.py
+++ diesel-old/manual-go/src/size-sensitivity-bscholes.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
+benchmarkName = 'bscholes'
+
 rawData = [(2000,1.74,4.30,2.04),(4000,3.73,7.16,4.08),(6000,5.36,10.09,5.82),(8000,6.55,12.65,7.00),(10000,8.76,17.76,11.46)]
 
 names = ['Baseline','Unoptimized','Optimized']
-linestyles = ['-','--','-.']
+linestyles = ['-','--',':']
 colors = ['orange','red','green']
 
 sizes = []
@@ -18,21 +20,25 @@ for datum in rawData:
   comms[1].append(120*Dim2/1e6)
   comms[2].append((40*Dim2+160)/1e6)
 
-plt.rcParams.update({'font.size': 18})
-
+plt.figure(figsize=(2,2))
+plt.rcParams.update({'font.size': 12})
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,times[i],label=names[i],linestyle=linestyles[i],color=colors[i])
-plt.legend(loc='upper left')
+# plt.legend(loc='upper left')
 plt.xlabel('Input Size')
 plt.ylabel('Time (ms)')
 plt.tight_layout()
-plt.savefig('times-bscholes.png')
+plt.savefig('times-{}.png'.format(benchmarkName))
 plt.close()
 
+plt.figure(figsize=(2,2))
+plt.rcParams.update({'font.size': 12})
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,comms[i],label=names[i],linestyle=linestyles[i],color=colors[i])
-plt.legend(loc='upper left')
+# plt.legend(loc='upper left')
 plt.xlabel('Input Size')
-plt.ylabel('Communicated Data (MB)')
+plt.ylabel('Data (MB)')
 plt.tight_layout()
-plt.savefig('comms-bscholes.png')
+plt.savefig('comms-{}.png'.format(benchmarkName))
--- diesel-old/manual-go/src/size-sensitivity-sobel.py
+++ diesel-old/manual-go/src/size-sensitivity-sobel.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
+benchmarkName = 'sobel'
+
 rawData = [(100,13.9,23.2,13.9),(200,47.5,82.8,52.6),(300,101.5,150.3,102.8),(400,167.8,319.4,170.2),(500,282.7,402.8,264.2)]
 
 names = ['Baseline','Unoptimized','Optimized']
-linestyles = ['-','--','-.']
+linestyles = ['-','--',':']
 colors = ['orange','red','green']
 
 sizes = []
@@ -18,21 +20,25 @@ for datum in rawData:
   comms[1].append(132*Dim2/1e6)
   comms[2].append((44*Dim2+160)/1e6)
 
-plt.rcParams.update({'font.size': 18})
-
+plt.figure(figsize=(2,2))
+plt.rcParams.update({'font.size': 12})
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,times[i],label=names[i],linestyle=linestyles[i],color=colors[i])
-plt.legend(loc='upper left')
+# plt.legend(loc='upper left')
 plt.xlabel('Input Size')
 plt.ylabel('Time (ms)')
 plt.tight_layout()
-plt.savefig('times-sobel.png')
+plt.savefig('times-{}.png'.format(benchmarkName))
 plt.close()
 
+plt.figure(figsize=(2,2))
+plt.rcParams.update({'font.size': 12})
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,comms[i],label=names[i],linestyle=linestyles[i],color=colors[i])
-plt.legend(loc='upper left')
+# plt.legend(loc='upper left')
 plt.xlabel('Input Size')
-plt.ylabel('Communicated Data (MB)')
+plt.ylabel('Data (MB)')
 plt.tight_layout()
-plt.savefig('comms-sobel.png')
+plt.savefig('comms-{}.png'.format(benchmarkName))
