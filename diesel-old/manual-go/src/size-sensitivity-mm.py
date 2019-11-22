fe2f29c01c7ba097001d7f13fd2b209dbd70731e
Keyur Joshi <kpjoshi2@illinois.edu>
2019-11-22 12:36:46 -0600

    update scripts and remove binaries

--- diesel-old/manual-go/src/size-sensitivity-mm.py
+++ diesel-old/manual-go/src/size-sensitivity-mm.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-benchmarkName = 'mm'
+benchmarkName = 'MatrixMult'
 
 rawData = [(50,7.7,12.2,7.8),(100,24.5,41.7,25.3),(150,52.2,88.8,55.6),(200,88.1,142.8,94.7),(250,135.2,219.0,143.2)]
 
@@ -20,25 +20,30 @@ for datum in rawData:
   comms[1].append(252*Dim2/1e6)
   comms[2].append((84*Dim2+240)/1e6)
 
-plt.figure(figsize=(2,2))
-plt.rcParams.update({'font.size': 12})
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,times[i],label=names[i],linestyle=linestyles[i],color=colors[i])
 # plt.legend(loc='upper left')
-plt.xlabel('Input Size')
-plt.ylabel('Time (ms)')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Time (ms)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
 plt.tight_layout()
 plt.savefig('times-{}.png'.format(benchmarkName))
-plt.close()
+plt.close('all')
 
-plt.figure(figsize=(2,2))
-plt.rcParams.update({'font.size': 12})
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,comms[i],label=names[i],linestyle=linestyles[i],color=colors[i])
 # plt.legend(loc='upper left')
-plt.xlabel('Input Size')
-plt.ylabel('Data (MB)')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Data (MB)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
 plt.tight_layout()
 plt.savefig('comms-{}.png'.format(benchmarkName))
+plt.close('all')
