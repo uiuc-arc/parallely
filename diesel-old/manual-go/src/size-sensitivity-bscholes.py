fa9136e3f37a3b3e9119201c996f6b5d23a96097
Keyur Joshi <kpjoshi2@illinois.edu>
2019-11-22 12:36:46 -0600

    update scripts and remove binaries

--- diesel-old/manual-go/src/size-sensitivity-bscholes.py
+++ diesel-old/manual-go/src/size-sensitivity-bscholes.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-benchmarkName = 'bscholes'
+benchmarkName = 'Blackscholes'
 
 rawData = [(2000,1.74,4.30,2.04),(4000,3.73,7.16,4.08),(6000,5.36,10.09,5.82),(8000,6.55,12.65,7.00),(10000,8.76,17.76,11.46)]
 
@@ -20,25 +20,30 @@ for datum in rawData:
   comms[1].append(120*Dim2/1e6)
   comms[2].append((40*Dim2+160)/1e6)
 
-plt.figure(figsize=(2,2))
-plt.rcParams.update({'font.size': 12})
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,times[i],label=names[i],linestyle=linestyles[i],color=colors[i])
 # plt.legend(loc='upper left')
-plt.xlabel('Input Size')
-plt.ylabel('Time (ms)')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Time (ms)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
 plt.tight_layout()
 plt.savefig('times-{}.png'.format(benchmarkName))
-plt.close()
+plt.close('all')
 
-plt.figure(figsize=(2,2))
-plt.rcParams.update({'font.size': 12})
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,comms[i],label=names[i],linestyle=linestyles[i],color=colors[i])
 # plt.legend(loc='upper left')
-plt.xlabel('Input Size')
-plt.ylabel('Data (MB)')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Data (MB)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
 plt.tight_layout()
 plt.savefig('comms-{}.png'.format(benchmarkName))
+plt.close('all')
--- diesel/src/scale-gen/plot_sensitivity.py
+++ diesel/src/scale-gen/plot_sensitivity.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-benchmarkName = 'scale'
+benchmarkName = 'Scale'
 
 data = {2: (2182202570.3, 2414151423.1, 2285356977.9, 8388608.0, 16777216.0, 8388672.0),
         4: (9446486885.3, 10332666758.0, 10303837240.5, 33554432.0, 67108864.0, 33554496.0),
@@ -24,52 +24,30 @@ for datum in sorted(data.keys()):
   comms[1].append(data[datum][4]/1e6)
   comms[2].append(data[datum][5]/1e6)
 
-plt.figure(figsize=(2,2))
-plt.rcParams.update({'font.size': 12})
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,times[i],label=names[i],linestyle=linestyles[i],color=colors[i])
 # plt.legend(loc='upper left')
-plt.xlabel('Input Size')
-plt.ylabel('Time (ms)')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Time (ms)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
 plt.tight_layout()
 plt.savefig('times-{}.png'.format(benchmarkName))
-plt.close()
+plt.close('all')
 
-plt.figure(figsize=(2,2))
-plt.rcParams.update({'font.size': 12})
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 for i in range(3):
   plt.plot(sizes,comms[i],label=names[i],linestyle=linestyles[i],color=colors[i])
 # plt.legend(loc='upper left')
-plt.xlabel('Input Size')
-plt.ylabel('Data (MB)')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Data (MB)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
 plt.tight_layout()
 plt.savefig('comms-{}.png'.format(benchmarkName))
-
-'''
-# fig, axs = plt.subplots(2, 1)
-for i in range(3):
-    plt.plot(sizes, times[i], label=names[i], linestyle=linestyles[i], color=colors[i])
-    # plt.plot(sizes, comms[i], label=names[i], linestyle=linestyles[i], color=colors[i])
-# for i in range(2):
-#   axs[i].legend(loc='upper left')
-#   axs[i].set_xlabel('Input Size')
-
-plt.legend(loc='upper left')
-plt.xlabel('Input Size')
-plt.ylabel('Time (s)')
-plt.xticks(sorted(data.keys()), ['2x', '4x', '8x', '16x'])
-plt.tight_layout()
-plt.savefig("time.png")
-plt.close("all")
-
-for i in range(3):
-    plt.plot(sizes, comms[i], label=names[i], linestyle=linestyles[i], color=colors[i])
-plt.legend(loc='upper left')
-plt.ylabel('Communicated Data (MB)')
-plt.xlabel('Input Size')
-plt.xticks(sorted(data.keys()), ['2x', '4x', '8x', '16x'])
-plt.tight_layout()
-plt.savefig("memory.png")
-'''
+plt.close('all')
