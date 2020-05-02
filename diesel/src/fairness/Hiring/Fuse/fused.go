51643654249337bc6d30419500ed79f6b0c5819d
vimuth <vimuth@gmail.com>
2020-05-02 13:06:48 -0500

    Vimuth

--- diesel/src/fairness/Hiring/Fuse/fused.go
+++ diesel/src/fairness/Hiring/Fuse/fused.go
@@ -19,22 +19,18 @@ var Q = []int {1,2,3,4,5,6,7,8};
 func func_Q(i int){
   defer diesel.Wg.Done();
 	fmt.Println("Starting workers");
-	var genders [] int 
+	var genders [1] int 
 
-	diesel.ReceiveIntArray(genders,i,0)
-
+	diesel.ReceiveIntArray(genders[:],i,0)
 
 	fmt.Println(genders)		//why is this not printing [2]??
-
-
-
 }
 
 func main() {
 
-  defer diesel.Wg.Done();
+  // defer diesel.Wg.Done();
 
-	var genders = [] int {2}	//a trivial array with a single element
+	var genders = [1] int {2}	//a trivial array with a single element
 
 	fmt.Println("Starting main thread");
 
@@ -51,12 +47,12 @@ func main() {
 	//send the data chunks to each processor
 //	for _, q := range Q {
 	for q := 1; q < 9; q++ {
-		diesel.SendIntArray(genders,0,q)
+		diesel.SendIntArray(genders[:],0,q)
 			
 	}
 
 	//diesel.Wg.Done()
-
+	diesel.Wg.Done();
 	diesel.Wg.Wait()
 
 
