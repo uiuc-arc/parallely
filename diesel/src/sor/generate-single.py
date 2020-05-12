014be37f64a04f78d93e4684e5bdfe7ddec8879f
Keyur Joshi <kpjoshi2@illinois.edu>
2020-05-12 11:34:46 -0500

    sor bench

---
+++ diesel/src/sor/generate-single.py
@@ -0,0 +1,23 @@
+import subprocess
+import re
+import numpy as np
+
+ArrayDim = 100
+ArraySz = ArrayDim*ArrayDim
+SliceSz = ArraySz/10
+
+template_str = open("sor.tmpl", 'r').readlines()
+with open("sor.par", "w") as fout:
+    for line in template_str:
+        newline = line.replace('__ARRAYDIM__', str(ArrayDim))
+        newline = newline.replace('__ARRAYSZ__', str(ArraySz))
+        newline = newline.replace('__SLICESZ__', str(SliceSz))
+        fout.write(newline)
+
+template_str = open("__basic_go.tmpl", 'r').readlines()
+with open("__basic_go.txt", "w") as fout:
+    for line in template_str:
+        newline = line.replace('__ARRAYDIM__', str(ArrayDim))
+        newline = newline.replace('__ARRAYSZ__', str(ArraySz))
+        newline = newline.replace('__SLICESZ__', str(SliceSz))
+        fout.write(newline)
