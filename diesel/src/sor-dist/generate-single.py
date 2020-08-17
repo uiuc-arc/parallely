119c20393d871a72da9a9ee804c1ea3e34f770de
vimuth <vimuth@gmail.com>
2020-08-17 18:44:08 -0500

    sopr-dist

---
+++ diesel/src/sor-dist/generate-single.py
@@ -0,0 +1,31 @@
+import subprocess
+import re
+import numpy as np
+
+ArrayDim = 100
+ArraySz = ArrayDim*ArrayDim
+SliceSz = ArraySz/10
+
+template_str = open("sor.tmpl", 'r').readlines()
+with open("sor.par", "w") as fout:
+    for line in template_str:
+        newline = line.replace('__ARRAYDIM__', str(ArrayDim))
+        newline = newline.replace('__ARRAYSZ__', str(ArraySz))
+        newline = newline.replace('__SLICESZ__', str(SliceSz))
+        fout.write(newline)
+
+template_str = open("__basic_go_main.tmpl", 'r').readlines()
+with open("__basic_go_main.txt", "w") as fout:
+    for line in template_str:
+        newline = line.replace('__ARRAYDIM__', str(ArrayDim))
+        newline = newline.replace('__ARRAYSZ__', str(ArraySz))
+        newline = newline.replace('__SLICESZ__', str(SliceSz))
+        fout.write(newline)
+
+template_str = open("__basic_go_worker.tmpl", 'r').readlines()
+with open("__basic_go_worker.txt", "w") as fout:
+    for line in template_str:
+        newline = line.replace('__ARRAYDIM__', str(ArrayDim))
+        newline = newline.replace('__ARRAYSZ__', str(ArraySz))
+        newline = newline.replace('__SLICESZ__', str(SliceSz))
+        fout.write(newline)        
