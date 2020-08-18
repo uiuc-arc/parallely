56d5a60502142826d4855106ccb483143860de42
vimuth <vimuth@gmail.com>
2020-08-18 03:09:48 -0500

    timing script for sobel

--- diesel/src/sobel/sobel-opt.go
+++ diesel/src/sobel/sobel-opt.go
@@ -4,7 +4,7 @@ import "fmt"
 import "math/rand"
 import "time"
 import "math"
-import "diesel"
+import "dieselacc"
 
 var Input_array [10000]float64
 var Num_threads int
@@ -13,138 +13,196 @@ var Q = []int {1,2,3,4,5,6,7,8,9,10};
 
 
 func func_0() {
-  defer diesel.Wg.Done();
-  var DynMap [11003]diesel.ProbInterval;
+  defer dieselacc.Wg.Done();
+  var DynMap [21003]float64;
+  var my_chan_index int;
+  _ = my_chan_index;
   _ = DynMap;
   var output [10000]float64;
-	var i int;
-	var idx int;
-	var temp1 float64;
-	var temp2 float32;
-	DynMap[0] = diesel.ProbInterval{1, 0};
-	var temp3 float32;
-	DynMap[1] = diesel.ProbInterval{1, 0};
-	var input32 [10000]float32;
-	diesel.InitDynArray(2, 10000, DynMap[:]);
-	var slice [1000]float32;
-	diesel.InitDynArray(10002, 1000, DynMap[:]);
-	var elem float64;
-	DynMap[11002] = diesel.ProbInterval{1, 0};
-	i = 0;
-	for __temp_0 := 0; __temp_0 < 10000; __temp_0++ {
-		_temp_index_1 := i;
-		temp1=Input_array[_temp_index_1];
-		temp2 = float32(temp1);
-		DynMap[0].Reliability = 0;
-		DynMap[0].Delta = diesel.GetCastingError64to32(temp1, temp2);
-		_temp_index_2 := i;
-		input32[_temp_index_2]=temp2;
-		DynMap[2 + _temp_index_2] = DynMap[0];
-		i = i+1;
-	}
-	for _, q := range(Q) {
-		diesel.SendDynFloat32ArrayO1(input32[:], 0, q, DynMap[:], 2);
-	}
-	for _, q := range(Q) {
-		diesel.ReceiveDynFloat32ArrayO1(slice[:], 0, q, DynMap[:], 10002);
-		i = 0;
-		for __temp_1 := 0; __temp_1 < 1000; __temp_1++ {
-			idx = (q-1)*100+i;
-			_temp_index_3 := i;
-			temp3=slice[_temp_index_3];
-			DynMap[1] = DynMap[10002 + _temp_index_3];
-			elem = float64(temp3);
-			DynMap[11002] = DynMap[1];
-			_temp_index_4 := idx;
-			output[_temp_index_4]=elem;
-			i = i+1;
-		}
-	}
+dieselacc.InitDynArray(0, 10000, DynMap[:]);
+var i int;
+var idx int;
+var size int;
+var perthread int;
+var temp1 float64;
+var temp2 float32;
+DynMap[10000] = 0.0;
+var temp3 float32;
+DynMap[10001] = 0.0;
+var input32 [10000]float32;
+dieselacc.InitDynArray(10002, 10000, DynMap[:]);
+var slice [1000]float32;
+dieselacc.InitDynArray(20002, 1000, DynMap[:]);
+var elem float64;
+DynMap[21002] = 0.0;
+i = 0;
+size = 10000;
+perthread = 1000;
+for __temp_0 := 0; __temp_0 < size; __temp_0++ {
+ _temp_index_1 := i;
+temp1=Input_array[_temp_index_1];
+temp2 = float32(temp1);
+DynMap[10000] = dieselacc.GetCastingError64to32(temp1, temp2);
+_temp_index_2 := i;
+input32[_temp_index_2]=temp2;
+DynMap[10002 + _temp_index_2] = DynMap[10000];
+i = i+1;
+ }
+for _, q := range(Q) {
+ dieselacc.SendDynFloat32ArrayO1(input32[:], 0, q, DynMap[:], 10002);
+ }
+for _, q := range(Q) {
+ dieselacc.ReceiveDynFloat32ArrayO1(slice[:], 0, q, DynMap[:], 20002);
+i = 0;
+for __temp_1 := 0; __temp_1 < perthread; __temp_1++ {
+ idx = (q-1)*100+i;
+_temp_index_3 := i;
+temp3=slice[_temp_index_3];
+DynMap[10001] = DynMap[20002 + _temp_index_3];
+elem = float64(temp3);
+DynMap[21002] = DynMap[10001];
+_temp_index_4 := idx;
+output[_temp_index_4]=elem;
+DynMap[0 + _temp_index_4] = DynMap[21002];
+i = i+1;
+ }
+ }
 
 
   fmt.Println("Ending thread : ", 0);
 }
 func func_Q(tid int) {
-  defer diesel.Wg.Done();
-  var DynMap [11009]diesel.ProbInterval;
+  defer dieselacc.Wg.Done();
+  var DynMap [22010]float64;
+  var my_chan_index int;
+  _ = my_chan_index;
   _ = DynMap;
   q := tid;
-	var array [10000]float32;
-	diesel.InitDynArray(0, 10000, DynMap[:]);
-	var slice [1000]float32;
-	diesel.InitDynArray(10000, 1000, DynMap[:]);
-	var i int;
-	var j int;
-	var k int;
-	var conditional int;
-	var point float32;
-	DynMap[11000] = diesel.ProbInterval{1, 0};
-	var temp1 float32;
-	DynMap[11001] = diesel.ProbInterval{1, 0};
-	var temp2 float32;
-	DynMap[11002] = diesel.ProbInterval{1, 0};
-	var temp3 float32;
-	DynMap[11003] = diesel.ProbInterval{1, 0};
-	var temp4 float32;
-	DynMap[11004] = diesel.ProbInterval{1, 0};
-	var temp5 float32;
-	DynMap[11005] = diesel.ProbInterval{1, 0};
-	var temp6 float32;
-	DynMap[11006] = diesel.ProbInterval{1, 0};
-	var temp7 float32;
-	DynMap[11007] = diesel.ProbInterval{1, 0};
-	var temp8 float32;
-	DynMap[11008] = diesel.ProbInterval{1, 0};
-	diesel.ReceiveDynFloat32ArrayO1(array[:], tid, 0, DynMap[:], 0);
-	i = (q-1)*10;
-	k = 0;
-	for __temp_2 := 0; __temp_2 < 10; __temp_2++ {
-		j = 1;
-		conditional = diesel.ConvBool((i<99)&&(i>0));
-		if conditional != 0 {
-			for __temp_3 := 0; __temp_3 < 98; __temp_3++ {
-				_temp_index_1 := i*100+j-101;
-				temp1=array[_temp_index_1];
-				_temp_index_2 := i*100+j-100;
-				temp2=array[_temp_index_2];
-				_temp_index_3 := i*100+j-99;
-				temp3=array[_temp_index_3];
-				_temp_index_4 := i*100+j+99;
-				temp4=array[_temp_index_4];
-				_temp_index_5 := i*100+j+100;
-				temp5=array[_temp_index_5];
-				_temp_index_6 := i*100+j+101;
-				temp6=array[_temp_index_6];
-				temp7 = temp2+temp2;
-				temp8 = temp5+temp5;
-				point = temp1+temp7;
-				point = point+temp3;
-				point = point-temp4;
-				point = point-temp8;
-				point = point-temp6;
-				DynMap[11000].Reliability = DynMap[11006].Reliability + DynMap[11000].Reliability + DynMap[11008].Reliability + DynMap[11008].Reliability + DynMap[11008].Reliability - 4.0;
-				DynMap[11000].Delta = DynMap[11000].Delta + DynMap[11006].Delta + DynMap[11000].Delta + DynMap[11000].Delta + DynMap[11000].Delta;
-				_temp_index_7 := k;
-				slice[_temp_index_7]=point;
-				DynMap[10000 + _temp_index_7] = DynMap[11000];
-				j = j+1;
-				k = k+1;
-			}
-		} else {
-			for __temp_4 := 0; __temp_4 < 98; __temp_4++ {
-				_temp_index_8 := i*100+j;
-				temp7=array[_temp_index_8];
-				DynMap[11007] = DynMap[0 + _temp_index_8];
-				_temp_index_9 := k;
-				slice[_temp_index_9]=temp7;
-				DynMap[10000 + _temp_index_9] = DynMap[11007];
-				j = j+1;
-				k = k+1;
-			}
-		}
-		i = i+1;
-	}
-	diesel.SendDynFloat32ArrayO1(slice[:], tid, 0, DynMap[:], 10000);
+var array [10000]float64;
+dieselacc.InitDynArray(0, 10000, DynMap[:]);
+var slice [1000]float64;
+dieselacc.InitDynArray(10000, 1000, DynMap[:]);
+var array32 [10000]float32;
+dieselacc.InitDynArray(11000, 10000, DynMap[:]);
+var slice32 [1000]float32;
+dieselacc.InitDynArray(21000, 1000, DynMap[:]);
+var i int;
+var j int;
+var c1 int;
+var c2 int;
+var k int;
+var cond1 int;
+var cond2 int;
+var conditional int;
+var point float64;
+DynMap[22000] = 0.0;
+var temp1 float64;
+DynMap[22001] = 0.0;
+var temp2 float64;
+DynMap[22002] = 0.0;
+var temp3 float64;
+DynMap[22003] = 0.0;
+var temp4 float64;
+DynMap[22004] = 0.0;
+var temp5 float64;
+DynMap[22005] = 0.0;
+var temp6 float64;
+DynMap[22006] = 0.0;
+var temp7 float64;
+DynMap[22007] = 0.0;
+var temp8 float64;
+DynMap[22008] = 0.0;
+var temp32 float32;
+DynMap[22009] = 0.0;
+dieselacc.ReceiveDynFloat32ArrayO1(array32[:], tid, 0, DynMap[:], 11000);
+i = 0;
+for __temp_2 := 0; __temp_2 < 10000; __temp_2++ {
+ _temp_index_1 := i;
+temp32=array32[_temp_index_1];
+DynMap[22009] = DynMap[11000 + _temp_index_1];
+temp1 = float64(temp32);
+DynMap[22001] = DynMap[22009];
+_temp_index_2 := i;
+array[_temp_index_2]=temp1;
+DynMap[0 + _temp_index_2] = DynMap[22001];
+i = i+1;
+ }
+i = (q-1)*10;
+k = 0;
+c1 = 10;
+c2 = 98;
+for __temp_3 := 0; __temp_3 < c1; __temp_3++ {
+ j = 1;
+cond1 = dieselacc.ConvBool(i<99);
+cond2 = dieselacc.ConvBool(i>0);
+conditional = dieselacc.ConvBool(cond1==1 && cond2==1);
+if conditional != 0 {
+ for __temp_4 := 0; __temp_4 < c2; __temp_4++ {
+ _temp_index_3 := i*100+j-101;
+temp1=array[_temp_index_3];
+DynMap[22001] = DynMap[0 + _temp_index_3];
+_temp_index_4 := i*100+j-100;
+temp2=array[_temp_index_4];
+DynMap[22002] = DynMap[0 + _temp_index_4];
+_temp_index_5 := i*100+j-99;
+temp3=array[_temp_index_5];
+DynMap[22003] = DynMap[0 + _temp_index_5];
+_temp_index_6 := i*100+j+99;
+temp4=array[_temp_index_6];
+DynMap[22004] = DynMap[0 + _temp_index_6];
+_temp_index_7 := i*100+j+100;
+temp5=array[_temp_index_7];
+DynMap[22005] = DynMap[0 + _temp_index_7];
+_temp_index_8 := i*100+j+101;
+temp6=array[_temp_index_8];
+DynMap[22006] = DynMap[0 + _temp_index_8];
+DynMap[22007] = DynMap[22002] + DynMap[22002];
+temp7 = temp2+temp2;
+DynMap[22008] = DynMap[22005] + DynMap[22005];
+temp8 = temp5+temp5;
+DynMap[22000] = DynMap[22001] + DynMap[22007];
+point = temp1+temp7;
+DynMap[22000] = DynMap[22000] + DynMap[22003];
+point = point+temp3;
+DynMap[22000] = DynMap[22000] + DynMap[22004];
+point = point-temp4;
+DynMap[22000] = DynMap[22000] + DynMap[22008];
+point = point-temp8;
+DynMap[22000] = DynMap[22000] + DynMap[22006];
+point = point-temp6;
+_temp_index_9 := k;
+slice[_temp_index_9]=point;
+DynMap[10000 + _temp_index_9] = DynMap[22000];
+j = j+1;
+k = k+1;
+ }
+ } else {
+ for __temp_5 := 0; __temp_5 < c2; __temp_5++ {
+ _temp_index_10 := i*100+j;
+temp7=array[_temp_index_10];
+DynMap[22007] = DynMap[0 + _temp_index_10];
+_temp_index_11 := k;
+slice[_temp_index_11]=temp7;
+DynMap[10000 + _temp_index_11] = DynMap[22007];
+j = j+1;
+k = k+1;
+ }
+ }
+i = i+1;
+ }
+i = 0;
+for __temp_6 := 0; __temp_6 < 1000; __temp_6++ {
+ _temp_index_12 := i;
+temp1=slice[_temp_index_12];
+DynMap[22001] = DynMap[10000 + _temp_index_12];
+temp32 = float32(temp1);
+DynMap[22009] = dieselacc.GetCastingError64to32(temp1, temp32);
+_temp_index_13 := i;
+slice32[_temp_index_13]=temp32;
+DynMap[21000 + _temp_index_13] = DynMap[22009];
+i = i+1;
+ }
+dieselacc.SendDynFloat32ArrayO1(slice32[:], tid, 0, DynMap[:], 21000);
 
   fmt.Println("Ending thread : ", q);
 }
@@ -154,7 +212,7 @@ func main() {
 
   Num_threads = 11;
 	
-	diesel.InitChannels(11);
+	dieselacc.InitChannels(11);
 
   // Using math becasue GoLang is crazy
   randSource := rand.NewSource(int64(math.Abs(1)))
@@ -167,13 +225,13 @@ func main() {
   startTime := time.Now()
 
   go func_0();
-	for _, index := range Q {
-		go func_Q(index);
-	}
+for _, index := range Q {
+go func_Q(index);
+}
 
 
   fmt.Println("Main thread waiting for others to finish");  
-	diesel.Wg.Wait()
+	dieselacc.Wg.Wait()
   elapsed := time.Since(startTime)
   
   fmt.Println("Elapsed time : ", elapsed.Nanoseconds())
