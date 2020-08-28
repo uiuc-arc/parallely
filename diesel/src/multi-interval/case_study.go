728c41ed373ede2c493b64b4a90e26ff0cf8d008
jacob <jlaurel2@illinois.edu>
2020-08-28 13:58:56 -0500

    saving updated benchmark

--- diesel/src/multi-interval/case_study.go
+++ diesel/src/multi-interval/case_study.go
@@ -3,6 +3,7 @@ package main
 import (
 	"../diesel"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -21,8 +22,16 @@ func convertToFloat(x int) float64 {
 
 
 func getVelocity() float64{
-	return 1.0
+	return 8+(5*rand.Float64())
+	//return 1.0
 }
+
+
+func getFinalVelocity() float64{
+	return 20+(3*rand.Float64())
+	//return 1.0
+}
+
 
 
 func getAcceleration() float64{
@@ -40,14 +49,16 @@ func func_0() {
 	var vel float64
 	var interval diesel.ProbInterval = diesel.ProbInterval{1, 0}
 	DynMap[0] = diesel.NewMultiInterval(vel,interval)
+	//DynMap[0].SetProbIntervalI(interval,0)
+	DynMap[0].PrintValues()
 
 	var acc float64
 	DynMap[1] = diesel.NewMultiInterval(acc,interval)
-
+	DynMap[1].PrintValues()
 
 	var vf float64
 	DynMap[2] = diesel.NewMultiInterval(vf,interval)
-
+	DynMap[2].PrintValues()
 
 	var dist float64
 	//DynMap[3] = diesel.NewMultiInterval(vf,interval)
@@ -55,13 +66,20 @@ func func_0() {
 	//read from sensor
 	vel = getVelocity()
 	DynMap[0].SetValueI(vel,0)
+	DynMap[0].SetDeltaI(10,0) //need to change back
+	DynMap[0].PrintValues()
 
 	//read from sensor
 	acc = getAcceleration()
 	DynMap[1].SetValueI(acc,0)
+	DynMap[1].SetDeltaI(0.2,0)
+
 
 	var min_vel,max_vel float64
-	max_vel,min_vel = diesel.MinMaxMultiInterval(DynMap[0])
+	min_vel,max_vel = diesel.MinMaxMultiInterval(DynMap[0])
+
+	fmt.Println(max_vel)
+	fmt.Println(min_vel)
 
 	//slow down
 	if (min_vel > 10.0) {
@@ -71,8 +89,12 @@ func func_0() {
 		acc = acc + 5.0
 		DynMap[1].SetValueI(acc+5.0,0) //set dynamic tracking for this value
 	} else {
+		fmt.Println("HERE")
 		DynMap[1].SetValueI(acc-5.0,0) //set dynamic tracking for both values
+
+		DynMap[1].AddOneMore(acc+5.0,DynMap[1].GetIntervalI(0))
 		DynMap[1].SetValueI(acc+5.0,1) //set dynamic tracking for both values
+
 		//set the concrete value
 		if (vel > 10.0) {
 			acc = acc - 5.0
@@ -83,7 +105,7 @@ func func_0() {
 
 
 	//read from sensor
-	vf = getVelocity()
+	vf = getFinalVelocity()
 	DynMap[2].SetValueI(vf,0)
 
 	
@@ -100,6 +122,8 @@ func func_0() {
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	fmt.Println("Starting main thread")
 	diesel.InitChannels(1)
 
