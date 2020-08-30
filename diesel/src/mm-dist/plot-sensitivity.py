1634686178120c2db72da48d6e77e1e8c03a5665
Keyur Joshi <kpjoshi2@illinois.edu>
2020-08-30 11:48:29 -0500

    add useful files

---
+++ diesel/src/mm-dist/plot-sensitivity.py
@@ -0,0 +1,51 @@
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+benchmarkName = 'MatrixMult'
+
+rawData = [(100,8.63,17.9,10.2),(120,11.3,23.7,14.6),(140,12.7,29.5,16.6),(160,17.7,40.7,24.5),(180,22.8,60.7,37.7),(200,26.7,78.4,46.3)]
+
+names = ['Baseline','Unoptimized','Optimized']
+linestyles = ['-','--',':']
+colors = ['orange','red','green']
+
+sizes = []
+times = ([],[],[])
+comms = ([],[],[])
+for datum in rawData:
+  sizes.append(datum[0])
+  for i in range(1,4):
+    times[i-1].append(datum[i])
+  Dim2 = datum[0]**2
+  comms[0].append(84*Dim2/1e6)
+  comms[1].append(252*Dim2/1e6)
+  comms[2].append((84*Dim2+240)/1e6)
+
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+for i in range(3):
+  plt.plot(sizes,times[i],label=names[i],linestyle=linestyles[i],color=colors[i])
+# plt.legend(loc='upper left')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Time (ms)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
+plt.tight_layout()
+plt.savefig('times-{}.png'.format(benchmarkName))
+plt.close('all')
+
+# plt.figure(figsize=(2,2))
+plt.title(benchmarkName, fontsize=20)
+# plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+for i in range(3):
+  plt.plot(sizes,comms[i],label=names[i],linestyle=linestyles[i],color=colors[i])
+# plt.legend(loc='upper left')
+plt.xlabel('Input Size', fontsize=20)
+plt.ylabel('Data (MB)', fontsize=20)
+plt.xticks(fontsize=20, rotation=90)
+plt.yticks(fontsize=20)
+plt.tight_layout()
+plt.savefig('comms-{}.png'.format(benchmarkName))
+plt.close('all')
