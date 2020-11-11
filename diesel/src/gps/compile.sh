28592b777939014f6566f1e794c07c3934588ae2
Keyur Joshi <kpjoshi2@illinois.edu>
2020-11-11 13:05:59 -0600

    fix gps bench

--- diesel/src/gps/compile.sh
+++ diesel/src/gps/compile.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-python ../../../parser/crosscompiler-diesel-dist.py -f gps.par -tm boilerplate_main.tmpl -tw boilerplate_main.tmpl -i -dyn -acc
+python ../../../parser/crosscompiler-diesel-dist.py -f gps.par -tm boilerplate_main.tmpl -tw boilerplate_worker.tmpl -i -dyn -acc
