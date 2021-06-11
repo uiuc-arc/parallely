429348d3ea8a8c3e3917f7389998124710cc044a
vimuth <wvf2@illinois.edu>
2021-06-10 21:31:30 -0500

    Fixed sssp. Removed old noisy send

---
+++ benchmarks/golang/src/sssp/run.sh
@@ -0,0 +1,24 @@
+benchmark=sssp
+
+cd ../../../../gofrontend/
+
+# Run translator
+echo "**************************************"
+echo "Running the translator Go -> Parallely"
+echo "**************************************"
+python -m newtranslator.translator.translator -f ../benchmarks/golang/src/$benchmark/$benchmark.go -o ../benchmarks/golang/src/$benchmark/$benchmark.par
+
+cd -
+# # Run sequentializer
+# echo "**************************************"
+# echo "Running the sequentializer"
+# echo "**************************************"
+# python ../../../../parser/compiler.py -f $benchmark.par -o $benchmark.seq
+
+# Run translator
+cd ../../../../gofrontend/
+echo "**************************************"
+echo "Generating executable code by renaming function calls with types (Go -> Go)."
+python -m newtranslator.translator.typedGoGenerator -f ../benchmarks/golang/src/$benchmark/$benchmark.go -o ../benchmarks/golang/src/$benchmark/$benchmark.exec.go
+echo "Use 'go run $benchmark.exec.go' to run the generated program" 
+echo "**************************************"
