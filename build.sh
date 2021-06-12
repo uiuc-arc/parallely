c39d01ad934e20b9f37c9fac20cfacd300ab9fee
Vimuth Fernando <wvf2@illinois.edu>
2021-06-12 15:47:26 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-ANTLRJAR=/home/vimuth/Downloads/antlr4-4.7.1-complete.jar
+ANTLRJAR=<Update Dir>
 
 cd gofrontend/newtranslator/antlrgenerated/
-java -Xmx500M -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python2 -visitor GoLexer.g4 
-java -Xmx500M -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python2 -visitor GoParser.g4
+java -Xmx500M -cp "$ANTLRJAR:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python2 -visitor GoLexer.g4 
+java -Xmx500M -cp "$ANTLRJAR:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python2 -visitor GoParser.g4
 
 cd -
 cd parser/
